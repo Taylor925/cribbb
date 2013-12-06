56c9329274cc12c5665bfc6c6a81ba4d393bcabf
David Taylor <dtaylor@archermalmo.com>
2013-12-06 10:25:50 -0600

    culttt.com/2013/05/13/setting-up-your-first-laravel-4-model/

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'cribbb',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'ghu89ijkm',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
